dc99d01057df90ca1e8fd91eb1d435d03b62393b
MissVeronica <veronica@tranza.se>
2022-03-21 16:24:47 +0100

    Create add-on-1.php

---
+++ add-on-1.php
@@ -0,0 +1,11 @@
+<?php
+add_filter( "um_mobile_number_form_edit_field", "um_mobile_number_form_edit_field_tel", 10, 2 );
+add_filter( "um_phone_number_form_edit_field", "um_mobile_number_form_edit_field_tel", 10, 2 );
+    
+    function um_mobile_number_form_edit_field_tel( $output, $set_mode ) {
+        
+        if( $set_mode == 'register' || $set_mode == 'profile' ) {
+            $output = str_replace( 'type="text"', 'type="tel"', $output );
+        }
+        return $output;
+    }
