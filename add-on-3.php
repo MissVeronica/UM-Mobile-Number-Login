751e0a941bebb8639e9becc653b2ce231c451577
MissVeronica <veronica@tranza.se>
2022-11-05 09:45:35 +0100

    Create add-on-3.php

---
+++ add-on-3.php
@@ -0,0 +1,10 @@
+add_filter( 'um_add_user_frontend_submitted', 'um_add_user_frontend_submitted_mobile_login', 10, 1 );
+
+function um_add_user_frontend_submitted_mobile_login( $args ) {
+
+    if( !isset( $args['user_login'] ) || empty( $args['user_login'] )) {
+        $args['user_login'] = $args['mobile_number'];
+        $args['submitted']['user_login'] = $args['mobile_number'];
+    }
+    return $args;
+}
